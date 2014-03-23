a4c14b90141d3bb55cdbf5cf7e86572425b42908
Hans-Helge Buerger <mail@hig-podcast.de>
2014-03-23 19:52:18 +0100

    fix aliases and add git aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,15 +1,26 @@
 # ---General---
-alias ...    = 'cd ../..'
-alias ....   = 'cd ../../..'
+alias ...= 'cd ../..'
+alias ....='cd ../../..'
 
 # ---Usefull Commmands---
-alias rot13  = 'php $DOTFILES/script/rot13.php'
-alias reload = 'source ~/.zshrc'
-alias gping  = 'ping google.de'
-alias week   = 'expr `date +%W` + 1'
+alias rot13='php $DOTFILES/script/rot13.php'
+alias reload='source ~/.zshrc'
+alias gping='ping google.de'
+alias week='expr `date +%W` + 1'
 
 # ---Programs---
-alias mou    = 'open -a Mou'
+alias mou='open -a Mou'
 
 # ---Shortcuts---
-alias code   = 'cd ~/Documents/code'
+alias code='cd ~/Documents/code'
+
+# ---GIT---
+alias g='git'
+alias glg='git lg'
+alias ga='git add'
+alias gc='git commit'
+alias amend='git commit --amend'
+alias gb='git branch'
+alias gco='git checkout'
+alias gm='git merge'
+
