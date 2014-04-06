c5966d35b823850b0aa16665ea3c0dd27c558b2a
Hans-Helge Buerger <mail@hig-podcast.de>
2014-04-06 18:37:25 +0200

    Fix, reload, speedtest, git status
    * fix ... alias
    * add clear to reload alias
    * add speedtest
    * add gs (git status) alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,21 +1,24 @@
 # ---General---
-alias ...= 'cd ../..'
+alias ...='cd ../..'
 alias ....='cd ../../..'
 
 # ---Usefull Commmands---
 alias rot13='php $DOTFILES/script/rot13.php'
-alias reload='source ~/.zshrc'
+alias reload='clear; source ~/.zshrc'
 alias gping='ping google.de'
 alias week='expr `date +%W` + 1'
 
 # ---Programs---
 alias mou='open -a Mou'
 alias 2048='sh $DOTFILES/script/bash2048.sh'
+alias speedtest='python ~/Documents/code/Python/tespeed/tespeed.py'
+
 # ---Shortcuts---
 alias code='cd ~/Documents/code'
 
 # ---GIT---
 alias g='git'
+alias gs='git status -s'
 alias glg='git lg'
 alias ga='git add'
 alias gc='git commit'
