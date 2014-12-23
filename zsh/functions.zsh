6cc74de5226fed398d4503b9737184ce20b8969f
Hans-Helge Buerger <mail@hanshelgebuerger.de>
2014-12-23 12:11:42 +0100

    cprint function
    this function continuously prints a given file and updates the view on screen if the file changes. Useful for demo purpose

---
+++ zsh/functions.zsh
@@ -0,0 +1,23 @@
+cprint() {
+	numRegex='[0-9]+$'
+	usrFile=$1
+	usrTime=$2
+	defLen=15
+	defTime=3
+
+	# Command line argument processing
+	if [[ $usrTime =~ $numRegex ]]; then
+		# A valid number of lines and refresh rate were passed in
+	else
+		# Invalid or no lines and refresh rate were provided
+		# so instead, using defaults instead.
+		usrLen=$defLen
+		usrTime=$defTime
+	fi
+
+	while [ true ]; do
+		clear
+		cat $usrFile
+		sleep $usrTime
+	done
+}
