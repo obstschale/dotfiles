2eda95dfceff46c2958fcbc2c90b560332f97552
Hans-Helge Buerger <mail@hanshelgebuerger.de>
2014-12-23 12:10:15 +0100

    alias vagrant-mysql and pubip

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -7,6 +7,8 @@ alias rot13='php $DOTFILES/script/rot13.php'
 alias reload='clear; source ~/.zshrc'
 alias gping='ping google.de'
 alias week='expr `date +%W` + 1'
+alias vagrant-mysql='vagrant ssh -c "sudo service mysql start"'
+alias pubip='curl ifconfig.me'
 alias teufel='ffplay -nodisp -loop 0 $DOTFILES/assets/sin_10Hz_-72dBFS.mp3 2>/dev/null &'
 # -I rc   open without gui
 alias swr3='vlc -I rc http://swr-mp3-m-swr3.akacast.akamaistream.net/7/720/137136/v1/gnl.akacast.akamaistream.net/swr-mp3-m-swr3'
