6bfca24246da7f9ad1f34642a2dba4a31b536ca6
Hans-Helge Buerger <mail@hanshelgebuerger.de>
2015-01-24 12:12:18 +0100

    Fritz Radio Stream als alias definiert

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -9,9 +9,12 @@ alias gping='ping google.de'
 alias week='expr `date +%W` + 1'
 alias vagrant-mysql='vagrant ssh -c "sudo service mysql start"'
 alias pubip='curl ifconfig.me'
+
+# ---Audio / Radio---
 alias teufel='ffplay -nodisp -loop 0 $DOTFILES/assets/sin_10Hz_-72dBFS.mp3 2>/dev/null &'
 # -I rc   open without gui
 alias swr3='vlc -I rc http://swr-mp3-m-swr3.akacast.akamaistream.net/7/720/137136/v1/gnl.akacast.akamaistream.net/swr-mp3-m-swr3'
+alias fritz='vlc -I rc http://rbb-mp3-fritz-m.akacast.akamaistream.net/7/799/292093/v1/gnl.akacast.akamaistream.net/rbb_mp3_fritz_m'
 
 # ---Programs---
 alias mou='open -a Mou'
@@ -19,9 +22,11 @@ alias 2048='sh $DOTFILES/script/bash2048.sh'
 alias speedtest='python ~/Documents/code/Python/tespeed/tespeed.py'
 alias ipn='ipython notebook'
 alias vlc='~/Applications/VLC.app/Contents/MacOS/VLC'
+alias cask='brew cask'
 
 export TODOTXT_DEFAULT_ACTION=ls
 alias t='todo.sh -d ~/.todo.cfg'
+alias tt='t today'
 
 # ---Shortcuts---
 alias code='cd ~/Documents/code'
