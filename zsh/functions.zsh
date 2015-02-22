71166916eebbefff73d112cae0acd6aa1147382f
Hans-Helge Buerger <mail@hanshelgebuerger.de>
2015-02-22 21:39:54 +0100

    add new todo card to GTD Trello board

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -21,3 +21,15 @@ cprint() {
 		sleep $usrTime
 	done
 }
+
+todo() {
+	gtd_id=`trello board list -o tsv | grep 00GTD | cut -d$'\t' -f 1`
+	inbox_id=`trello list list -b $gtd_id -o tsv | grep Inbox | cut -d$'\t' -f 1`
+
+	if [ -z ${2+x} ]; then
+		trello card create -l $inbox_id -n $1
+	else
+		trello card create -l $inbox_id -n $1 -d $2
+	fi
+
+}
