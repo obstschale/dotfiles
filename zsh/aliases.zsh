420314deb5f3fa2b5057fa26b0cac0070ff5ebc8
Hans-Helge Buerger <mail@hanshelgebuerger.de>
2017-10-12 16:35:25 +0200

    Add new aliases and radio station list for pyradio

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,36 +1,47 @@
 # ---General---
 alias ...='cd ../..'
 alias ....='cd ../../..'
+alias ll='ls -lh'
+alias la='ls -lah'
+alias l='la'
+
+# ---Core---
+alias showFiles='defaults write com.apple.finder AppleShowAllFiles TRUE; killall Finder'
+alias hideFiles='defaults write com.apple.finder AppleShowAllFiles FALSE; killall Finder'
 
 # ---Usefull Commmands---
 alias rot13='php $DOTFILES/script/rot13.php'
-alias reload='clear; source ~/.zshrc'
+alias reload='clear; source $HOME/.zshrc'
 alias gping='ping google.de'
-alias week='expr `date +%W` + 1'
+alias dping='ping duckduckgo.com'
+alias week='expr `date +%W`'
 alias vagrant-mysql='vagrant ssh -c "sudo service mysql start"'
 alias pubip='curl ifconfig.me'
+alias wetter='curl -4 wttr.in/Berlin'
+alias wttr='curl -s wttr.in/Berlin | head -7 | tail -5'
+alias ssh-pass='ssh -o PubkeyAuthentication=no'
+alias art='php artisan'
+alias json='python -mjson.tool'
+alias jsonc='json | pygmentize -l json'
 
 # ---Audio / Radio---
 alias teufel='ffplay -nodisp -loop 0 $DOTFILES/assets/sin_10Hz_-72dBFS.mp3 2>/dev/null &'
 # -I rc   open without gui
-alias swr3='vlc -I rc http://swr-mp3-m-swr3.akacast.akamaistream.net/7/720/137136/v1/gnl.akacast.akamaistream.net/swr-mp3-m-swr3'
+alias swr3='vlc -I rc http://mp3-live.swr3.de/swr3_m.m3u'
 alias fritz='vlc -I rc http://rbb-mp3-fritz-m.akacast.akamaistream.net/7/799/292093/v1/gnl.akacast.akamaistream.net/rbb_mp3_fritz_m'
 alias fluxfm='vlc -I rc http://stream.hoerradar.de/fluxfm-berlin'
 
 # ---Programs---
-alias mou='open -a Mou'
 alias 2048='sh $DOTFILES/script/bash2048.sh'
-alias speedtest='python ~/Documents/code/Python/tespeed/tespeed.py'
-alias ipn='ipython notebook'
-alias vlc='~/Applications/VLC.app/Contents/MacOS/VLC'
+alias jup='jupyter notebook'
+alias vlc='/Applications/VLC.app/Contents/MacOS/VLC'
 alias cask='brew cask'
-
-export TODOTXT_DEFAULT_ACTION=ls
-alias t='todo.sh -d ~/.todo.cfg'
-alias tt='t today'
+alias phpstorm='open -a /Applications/PhpStorm.app "`pwd`"'
+alias pyradio='pyradio --stations $DOTFILES/assets/pyradio_stations.csv'
 
 # ---Shortcuts---
-alias code='cd ~/Documents/code'
+alias code='cd $HOME/Code'
+alias projects='cd $HOME/Projects'
 
 # ---GIT---
 alias g='git'
@@ -42,4 +53,3 @@ alias amend='git commit --amend'
 alias gb='git branch'
 alias gco='git checkout'
 alias gm='git merge'
-
