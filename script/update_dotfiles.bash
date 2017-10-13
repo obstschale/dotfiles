031c64a2a2f7c2148bf79310f3e687f575ff19fc
Hans-Helge Buerger <mail@hanshelgebuerger.de>
2017-10-13 17:41:40 +0200

    Move color function to helper.bash

--- script/update_dotfiles.bash
+++ script/update_dotfiles.bash
@@ -1,28 +1,5 @@
 #!/usr/bin/env bash
 
-function _enable_colors() {
-    # Use colors, but only if connected to a terminal, and that terminal
-    # supports them.
-    if which tput >/dev/null 2>&1; then
-        ncolors=$(tput colors)
-    fi
-    if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
-      RED="$(tput setaf 1)"
-      GREEN="$(tput setaf 2)"
-      YELLOW="$(tput setaf 3)"
-      BLUE="$(tput setaf 4)"
-      BOLD="$(tput bold)"
-      NORMAL="$(tput sgr0)"
-    else
-      RED=""
-      GREEN=""
-      YELLOW=""
-      BLUE=""
-      BOLD=""
-      NORMAL=""
-    fi
-}
-
 function _current_epoch() {
   echo $(( $(date '+%s') / 60 / 60 / 24 ))
 }
