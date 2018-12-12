be40b89932c2be0a9ebf5c2d60657552417a6170
Hans-Helge Buerger <mail@hanshelgebuerger.de>
2018-12-12 21:22:45 +0100

    Add new aliases for git, radio, cat, ping

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -12,34 +12,45 @@ alias hideFiles='defaults write com.apple.finder AppleShowAllFiles FALSE; killal
 # ---Usefull Commmands---
 alias rot13='php $DOTFILES/script/rot13.php'
 alias reload='clear; source $HOME/.zshrc'
-alias dping='ping duckduckgo.com'
 alias week='expr `date +%W`'
-alias vagrant-mysql='vagrant ssh -c "sudo service mysql start"'
 alias pubip='curl ifconfig.me'
-alias wetter='curl -4 wttr.in/Berlin'
-alias wttr='curl -s wttr.in/Berlin | head -7 | tail -5'
 alias ssh-pass='ssh -o PubkeyAuthentication=no'
 alias art='php artisan'
 alias json='python -mjson.tool'
 alias jsonc='json | pygmentize -l json'
+alias cat='bat'
+alias dping='ping duck.co'
+alias ping='prettyping --nolegend'
 
 # ---Audio / Radio---
 alias teufel='ffplay -nodisp -loop 0 $DOTFILES/assets/sin_10Hz_-72dBFS.mp3 2>/dev/null &'
+#################################
+# Play radio with pyradio.
+#################################
+alias pyradio='pyradio --stations $DOTFILES/assets/pyradio_stations.csv'
+alias radio='pyradio'
+alias nova='radio -p 1'
+# alias swr3='radio -p 2'
+
+#################################
+# Play webradio streams with VLC
 # -I rc   open without gui
+#################################
 alias swr3='vlc -I rc http://mp3-live.swr3.de/swr3_m.m3u'
-alias fritz='vlc -I rc http://rbb-mp3-fritz-m.akacast.akamaistream.net/7/799/292093/v1/gnl.akacast.akamaistream.net/rbb_mp3_fritz_m'
-alias fluxfm='vlc -I rc http://stream.hoerradar.de/fluxfm-berlin'
+# alias fritz='vlc -I rc http://rbb-mp3-fritz-m.akacast.akamaistream.net/7/799/292093/v1/gnl.akacast.akamaistream.net/rbb_mp3_fritz_m'
+# alias fluxfm='vlc -I rc http://stream.hoerradar.de/fluxfm-berlin'
 
 # ---Programs---
 alias 2048='sh $DOTFILES/script/bash2048.sh'
-alias jup='jupyter notebook'
 alias vlc='/Applications/VLC.app/Contents/MacOS/VLC'
 alias cask='brew cask'
 alias phpstorm='open -a /Applications/PhpStorm.app "`pwd`"'
-alias pyradio='pyradio --stations $DOTFILES/assets/pyradio_stations.csv'
+alias ct='composer test'
+alias cov='composer coverage'
+alias naturemood='$DOTFILES/script/naturemood'
 
 # ---Shortcuts---
-alias code='cd $HOME/Code'
+# alias code='cd $HOME/Code'
 alias projects='cd $HOME/Projects'
 
 # ---GIT---
@@ -54,3 +65,6 @@ alias gco='git checkout'
 alias gm='git merge'
 alias gloc='git ls-files | xargs cat | wc -l'
 alias glocf='git ls-files | xargs wc -l'
+alias gdis='git discard'
+alias gfmd='git fetch -p && gm && g prune-remote'
+alias nah='git reset --hard'
